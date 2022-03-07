53a4d902c5e96eb4d9b0f3df1db744428abe0b27
TheTrueLando <45887470+TheTrueLando@users.noreply.github.com>
2022-03-06 20:36:13 -0600

    Update Program.cs

--- LandoNet/Program.cs
+++ LandoNet/Program.cs
@@ -1,19 +1,60 @@
-﻿Console.Title = "LandoNet";
+Console.Title = "LandoNet";
 
 string skillIssue;
-string usa;
 string instructions;
 
+//Current Commands
+string[] cmds = {"usa", "treetrain1", "math"};
+
+
 //Begin Operations
-Console.WriteLine("How can I help you? You can say cmds for a list of commands!: ");
+Console.WriteLine("How can I help you? You can say cmds for a list of commands!:");
 instructions = Console.ReadLine();
 
-if (instructions == "cmds") {
-    Console.WriteLine("treerain1\n usa");
-    Console.WriteLine("How can I help you? You can say cmds for a list of commands!: ");
-}
+
 //Begin Commands
+//cmds Command
+
+if (instructions == "cmds") {
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("--------------------------------------");
+    Console.ForegroundColor = ConsoleColor.Green;
+    for (int i = 0; i < cmds.Length; i++)
+    {
+        Console.WriteLine(cmds[i]);
+    }
+}
+
+//Calculator Command
+
+if (instructions == "math") {
+    Console.WriteLine("Type first number...");
+		int x = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Type...\n a for adding\n s for subtract\n m for multiply\n d for divide");
+		string z = Console.ReadLine();
+		Console.WriteLine("Type second number...");
+		int y = Convert.ToInt32(Console.ReadLine());
+		if (z == "a")
+		{
+			Console.WriteLine("Answer: {0}",x+y);
+		}
+		if (z == "s")
+		{
+			Console.WriteLine("Answer: {0}",x-y);
+		}
+		if (z == "m")
+		{
+			Console.WriteLine("Answer: {0}",x*y);
+		}
+		if (z == "d")
+		{
+			Console.WriteLine("Answer: {0}",x/y);
+		}
+		   
+}
+
 //USA Command
+
 else if (instructions == "usa") {
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine("WE");
@@ -25,7 +66,8 @@ else if (instructions == "usa") {
     Console.WriteLine("GUNS");
 }
 
-//treetrain1 command
+//treetrain1 Command
+
 else if (instructions == "treetrain1") {
     Console.WriteLine("Does treetrain1 have a skill issue?: ");
 skillIssue = Console.ReadLine();
@@ -37,25 +79,23 @@ else if (skillIssue == "no") {
 }
 else {
     Console.WriteLine("Try saying yes or no. I dont know what you typed!");
+    Console.ReadLine();
 }
 }
 
 
 
-//Unknown Command
+
+
 else {
-    Console.WriteLine("!!!\n Unknown Command, there is nothing else I can do.");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Unknown command, please try again.");
 }
-
-
-
-
-
-
 
 // Ending terminal
 //System.Threading.Thread.Sleep(2000);
-Console.ForegroundColor = ConsoleColor.Red;
-Console.WriteLine(".........................................Operations Complete.........................................");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(".........................................All Operations Have Been Completed.........................................");
+Console.WriteLine(".........................................You may now close this terminal.........................................");
 Console.WriteLine("");
 Console.ReadKey();
